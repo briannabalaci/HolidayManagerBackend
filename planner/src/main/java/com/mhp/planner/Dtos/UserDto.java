eaaf77019deae5793d5d9e7b60b180548f954170
DESKTOP-CJTKSNG\TestIntern02 <Mihai-Robert.Mehi@mhp.com>
2021-08-03 15:12:39 +0300

    Made some changes to user DTO

--- planner/src/main/java/com/mhp/planner/Dtos/UserDto.java
+++ planner/src/main/java/com/mhp/planner/Dtos/UserDto.java
@@ -17,9 +17,9 @@ public class UserDto {
 
     private String password;
 
-    private RoleDto role;
+    private String role;
 
-    private DepartmentDto department;
+    private String department;
 
 
 
