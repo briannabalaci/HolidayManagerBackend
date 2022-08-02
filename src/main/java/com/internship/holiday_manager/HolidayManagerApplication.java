869d500014807ea9f769e97a505fb8f54217741a
Mihaela-Alexandra Bledea <mihaela-alexandra.bledea@mhp.com>
2022-08-02 12:41:06 +0300

    Project Initialization

---
+++ src/main/java/com/internship/holiday_manager/HolidayManagerApplication.java
@@ -0,0 +1,13 @@
+package com.internship.holiday_manager;
+
+		import org.springframework.boot.SpringApplication;
+		import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HolidayManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HolidayManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/internship/holiday_manager/HolidayManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.internship.holiday_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HolidayManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
