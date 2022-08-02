f808691bcc17d24402ba1ec4f37f5745bbf12f35
DESKTOP-CJTKSNG\TestIntern02 <tara-adela.hudrea@mhp.com>
2022-08-02 15:06:59 +0300

    Created User entity

--- src/main/java/com/internship/holiday_manager/HolidayManagerApplication.java
+++ src/main/java/com/internship/holiday_manager/HolidayManagerApplication.java
@@ -1,7 +1,7 @@
 package com.internship.holiday_manager;
 
-		import org.springframework.boot.SpringApplication;
-		import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class HolidayManagerApplication {
