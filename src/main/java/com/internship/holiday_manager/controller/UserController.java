d30611a10039645b6b9c9d688da1a9babeacd77c
Mihaela-Alexandra Bledea <mihaela-alexandra.bledea@mhp.com>
2022-08-03 10:45:26 +0300

    Added a dto for the login credentials of the user.

--- src/main/java/com/internship/holiday_manager/controller/UserController.java
+++ src/main/java/com/internship/holiday_manager/controller/UserController.java
@@ -1,6 +1,7 @@
 package com.internship.holiday_manager.controller;
 
 
+import com.internship.holiday_manager.dto.LoginUserDto;
 import com.internship.holiday_manager.dto.UserDto;
 import com.internship.holiday_manager.entity.User;
 import com.internship.holiday_manager.service.UserService;
@@ -20,7 +21,7 @@ public class UserController {
     }
 
     @PostMapping("/login")
-    public ResponseEntity<User> authentication(@RequestBody UserDto dto){
+    public ResponseEntity<User> authentication(@RequestBody LoginUserDto dto){
         return new ResponseEntity<>(userService.authentication(dto.getEmail(), dto.getPassword()), HttpStatus.OK);
     }
 }
---
+++ src/main/java/com/internship/holiday_manager/mapper/LoginUserMapper.java
@@ -0,0 +1,21 @@
+package com.internship.holiday_manager.mapper;
+
+import com.internship.holiday_manager.dto.LoginUserDto;
+import com.internship.holiday_manager.dto.UserDto;
+import com.internship.holiday_manager.entity.User;
+import org.mapstruct.Mapper;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring")
+public interface LoginUserMapper {
+
+    LoginUserDto entityToDto(User entity);
+
+    User dtoToEntity(LoginUserDto dto);
+
+    List<LoginUserDto> entitiesToDtos(List<User> entities);
+
+    List<User> dtosToEntities(List<LoginUserDto> dtos);
+
+}
