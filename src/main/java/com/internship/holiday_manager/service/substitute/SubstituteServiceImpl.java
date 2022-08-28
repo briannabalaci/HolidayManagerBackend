c3efc13fe013e74aa7c9769ee68f9c2bef34df5a
DESKTOP-CJTKSNG\TestIntern02 <brianna.balaci@mhp.com>
2022-08-29 01:04:46 +0300

    get replacement

--- src/main/java/com/internship/holiday_manager/service/substitute/SubstituteServiceImpl.java
+++ src/main/java/com/internship/holiday_manager/service/substitute/SubstituteServiceImpl.java
@@ -2,10 +2,12 @@ package com.internship.holiday_manager.service.substitute;
 
 import com.internship.holiday_manager.dto.substitute.SubstituteDto;
 import com.internship.holiday_manager.dto.user.UserDto;
+import com.internship.holiday_manager.dto.user.UserWithTeamIdDto;
 import com.internship.holiday_manager.entity.Substitute;
 import com.internship.holiday_manager.entity.User;
 import com.internship.holiday_manager.mapper.SubstituteMapper;
 import com.internship.holiday_manager.mapper.UserMapper;
+import com.internship.holiday_manager.mapper.UserWithTeamIdMapper;
 import com.internship.holiday_manager.repository.SubstituteRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -22,11 +24,13 @@ public class SubstituteServiceImpl implements SubstituteService {
 
     private final SubstituteRepository substituteRepository;
     private final SubstituteMapper substituteMapper;
+    private final UserWithTeamIdMapper userWithTeamIdMapper;
     private final UserMapper userMapper;
 
-    public SubstituteServiceImpl(SubstituteRepository substituteRepository, SubstituteMapper substituteMapper, UserMapper userMapper) {
+    public SubstituteServiceImpl(SubstituteRepository substituteRepository, SubstituteMapper substituteMapper, UserWithTeamIdMapper userWithTeamIdMapper, UserMapper userMapper) {
         this.substituteRepository = substituteRepository;
         this.substituteMapper = substituteMapper;
+        this.userWithTeamIdMapper = userWithTeamIdMapper;
         this.userMapper = userMapper;
     }
 
@@ -47,4 +51,11 @@ public class SubstituteServiceImpl implements SubstituteService {
                 .collect(Collectors.toList());
         return userMapper.entitiesToDtos(teamLeaders);
     }
+
+    public UserDto getSubstituteOfTeamLead(UserWithTeamIdDto userDto) {
+        User user = userWithTeamIdMapper.dtoToEntity(userDto);
+        User rez = substituteRepository.findByTeamLead(user).get(0).getSubstitute();
+        return userMapper.entityToDto(rez);
+    }
+
 }
